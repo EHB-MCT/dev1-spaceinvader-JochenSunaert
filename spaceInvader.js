1b508ae99447684d769a0363fe4b767b6f829d86
Jochen Sunaert <Jochen.sunaert@student.ehb.be>
2022-10-05 21:45:14 +0200

    Space invader created in js.

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,36 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+
+function drawSpaceInvader() {
+    //SPACE INVADER
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.lineWidth=3;
+    context.fillStyle="white"
+    context.fill();
+
+    document.body.style.backgroundColor = "black";
+
+    context.beginPath();
+    context.rect(125, 75, 150, 50);
+    context.rect(75, 175, 50, 50);
+    context.rect(175, 175, 50, 50);
+    context.rect(275, 175, 50, 50);
+    context.rect(225, 225, 50, 50);
+    context.rect(125, 225, 50, 50);
+    context.rect(75, 275, 50, 50);
+    context.rect(175, 275, 50, 50);
+    context.rect(275, 275, 50, 50);
+    context.fillStyle="purple"
+    context.fill();
+
+
+}
+
+drawSpaceInvader();
+
